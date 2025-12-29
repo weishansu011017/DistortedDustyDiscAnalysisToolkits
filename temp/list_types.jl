96b972504e250c583c6f6a6a4d1d23d2435aca81
Wei-Shan Su <weishansu011017@gmail.com>
2025-12-30 02:06:54 +0800

    Remove temp

--- temp/list_types.jl
+++
@@ -1,8 +0,0 @@
-using PhantomRevealer
-
-const IO = PhantomRevealer.IO
-
-prdfs = IO.read_phantom(joinpath("test", "testinput", "testdumpfile_00000"); separate_types = :all)
-println("count: ", length(prdfs))
-println("types: ", [get(prdf.params, :itype, missing) for prdf in prdfs])
-println("npart: ", [IO.get_npart(prdf) for prdf in prdfs])
--- temp/run_grid_tests.jl
+++
@@ -1,2 +0,0 @@
-using Test
-include(joinpath(@__DIR__, "..", "test", "grid_interpolation_tests.jl"))
